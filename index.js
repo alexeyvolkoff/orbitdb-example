54a9b7bfaaac0d506666476d85a97bd353967cd9
Alexey Volkov <alexey.volkov@bineon.team>
2024-01-27 12:47:05 +0100

    bootstrap peer discovery

--- index.js
+++ index.js
@@ -1,50 +1,74 @@
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
 import { circuitRelayServer } from '@libp2p/circuit-relay-v2'
+import { tcp } from '@libp2p/tcp'
+import { mdns } from '@libp2p/mdns'
 import { identify } from '@libp2p/identify'
+import { LevelBlockstore } from 'blockstore-level'
 import { mplex } from '@libp2p/mplex'
-import { webSockets } from '@libp2p/websockets'
-import { webRTC } from '@libp2p/webrtc'
-import * as filters  from '@libp2p/websockets/filters'
 import { createLibp2p } from 'libp2p'
 import { gossipsub } from '@chainsafe/libp2p-gossipsub'
+import { webSockets } from '@libp2p/websockets'
+import { bootstrap } from '@libp2p/bootstrap'
+import * as filters from '@libp2p/websockets/filters'
 import { circuitRelayTransport } from '@libp2p/circuit-relay-v2'
 import { createOrbitDB, IPFSAccessController } from '@orbitdb/core'
 import { createHelia } from 'helia'
 
 const main = async () => {  
+
+    // Known peers addresses
+    const bootstrapMultiaddrs = [
+        '/ip4/172.20.64.111/tcp/34617/p2p/12D3KooWPxFA6YEeo2nhExuHKjEBDUZojj8W76kbmipY5zb9tmV6',
+        '/ip4/172.20.64.111/tcp/37587/ws/p2p/12D3KooWPxFA6YEeo2nhExuHKjEBDUZojj8W76kbmipY5zb9tmV6',
+    ]
+
     const Libp2pOptions = {
-        addresses: {
-            listen: ['/ip4/127.0.0.1/tcp/0/ws']
-        },
-        transports: [
-            webSockets({
-                filter: filters.all
+        peerDiscovery: [
+            mdns(),
+            bootstrap({
+                list: bootstrapMultiaddrs, // provide array of multiaddrs
             })
         ],
+        addresses: {
+            listen: ['/ip4/0.0.0.0/tcp/0',
+                     '/ip4/0.0.0.0/tcp/0/ws',
+            ]
+        },
+        transports: [
+            tcp(),
+            webSockets({
+                filter: filters.all
+            }),
+        ],
         connectionEncryption: [noise()],
-        streamMuxers: [yamux(), mplex()],
+        streamMuxers: [yamux()],
         services: {
             identify: identify(),
-            relay: circuitRelayServer({
-                reservations: {
-                    maxReservations: Infinity
-                }
-            }),
             pubsub: gossipsub({ allowPublishToZeroPeers: true })
-        },
-        connectionManager: {
-            minConnections: 0
         }
     }
 
     const libp2p = await createLibp2p(Libp2pOptions)
+
+
+    libp2p.addEventListener('peer:discovery', (evt) => {
+        console.log('Discovered %s', evt.detail.id.toString()) // Log discovered peer
+    })
+
+    libp2p.addEventListener('peer:connect', (evt) => {
+        console.log('Connected to %s', evt.detail.toString()) // Log connected peer
+    })
+
+    const blockstore = new LevelBlockstore('./ipfs')
+
     const ipfs = await createHelia({ libp2p })
 
     // create a random directory to avoid OrbitDB conflicts.
     let randDir = (Math.random() + 1).toString(36).substring(2)
 
-    const orbitdb = await createOrbitDB({ ipfs, directory: `./${randDir}/orbitdb` })
+    const orbitdb = await createOrbitDB({ ipfs, directory: `./ipfs/${randDir}/orbitdb` })
+    //const orbitdb = await createOrbitDB({ ipfs, directory: './ipfs/orbitdb' })
 
     let db
 
@@ -52,39 +76,42 @@ const main = async () => {
         db = await orbitdb.open(process.argv[2])
     } else {
         // When we open a new database, write access is only available to the
-        // db creator. When replicating a database on a remote peer, the remote
-        // peer must also have write access. Here, we are simply allowing anyone
-        // to write to the database. A more robust solution would use the
-        // OrbitDBAccessController to provide "fine-grain" access using grant and
-        // revoke.
-        db = await orbitdb.open('my-db', { AccessController: IPFSAccessController({ write: ['*']})})
+        // db creator. If we want to allow other peers to write to the database,
+        // they must be specified in IPFSAccessController write array param. Here,
+        // we simply allow anyone to write to the database. A more robust solution
+        // would use the OrbitDBAccessController to provide mutable, "fine-grain"
+        // access using grant and revoke.
+        db = await orbitdb.open('my-db', { AccessController: IPFSAccessController({ write: ['*']}) })
+
+        // Copy this output if you want to connect a peer to another.
+        console.log('my-db address', '(copy my db address and use when launching peer 2)', db.address)
+        console.log('my-db peer address', libp2p.getMultiaddrs().toString());
     }
 
-  // Copy this output if you want to connect a peer to another.
-  console.log('my-db address', db.address)
+    db.events.on('update', async (entry) => {
+        // what has been updated.
+        console.log('update', entry.payload.value)
+    })
 
-  // Add some records to the db when another peers joins.
-  db.events.on('join', async (peerId, heads) => {
-    await db.add('hello world 1')
-    await db.add('hello world 2')
-  })
+    if (process.argv[2]) {
+        await db.add('hello from second peer')
+        await db.add('hello again from second peer')
+    } else {
+        // write some records
+        await db.add('hello from first peer')
+        await db.add('hello again from first peer')
+    }
+    // Clean up when stopping this app using ctrl+c
+    process.on('SIGINT', async () => {
+        // print the final state of the db.
+        console.log((await db.all()).map(e => e.value))
+        // Close your db and stop OrbitDB and IPFS.
+        await db.close()
+        await orbitdb.stop()
+        await ipfs.stop()
 
-  db.events.on('update', async (entry) => {
-    console.log('entry', entry)
-    
-    // To complete full replication, fetch all the records from the other peer.
-    await db.all()
-  })
-
-  // Clean up when stopping this app using ctrl+c
-  process.on('SIGINT', async () => {
-      // Close your db and stop OrbitDB and IPFS.
-      await db.close()
-      await orbitdb.stop()
-      await ipfs.stop()
-
-      process.exit()
-  })
+        process.exit()
+    })
 }
 
 main()

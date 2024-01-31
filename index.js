28a880e6a598e27fce32bce19ac12e0bef4fff42
Alexey Volkov <alexey.volkov@bineon.team>
2024-01-31 08:43:19 +0100

    put CID to db record and retreive content on replication

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ import { autoNAT } from '@libp2p/autonat'
 import { gossipsub } from '@chainsafe/libp2p-gossipsub'
 import { pubsubPeerDiscovery } from '@libp2p/pubsub-peer-discovery'
 import { webSockets } from '@libp2p/websockets'
+import { webTransport } from '@libp2p/webtransport'
+import { webRTC } from '@libp2p/webrtc'
 import { bootstrap } from '@libp2p/bootstrap'
 import { kadDHT, removePrivateAddressesMapper } from '@libp2p/kad-dht'
 import * as filters from '@libp2p/websockets/filters'
@@ -59,6 +61,7 @@ const main = async () => {
         addresses: {
             listen: ['/ip4/0.0.0.0/tcp/3303',
                      '/ip4/0.0.0.0/tcp/3304/ws',
+                     '/webrtc'	
             ]
         },
         transports: [
@@ -66,6 +69,17 @@ const main = async () => {
             webSockets({
                 filter: filters.all
             }),
+            webRTC({
+                rtcConfiguration: {
+	        iceServers:[{
+         	 	urls: [
+            			'stun:stun.l.google.com:19302',
+            			'stun:global.stun.twilio.com:3478'
+          		]
+        	  }]
+    		}
+	    }),
+	    webTransport()
         ],
         connectionEncryption: [noise()],
         streamMuxers: [yamux()],
@@ -75,7 +89,7 @@ const main = async () => {
             pubsub: gossipsub({ allowPublishToZeroPeers: true }),
             dht: kadDHT({
                 kBucketSize: 20,
-                clientMode: true,
+                clientMode: false,
                 enabled: true,
                 randomWalk: {
                     enabled: true,            // Allows to disable discovery (enabled by default)
@@ -86,6 +100,7 @@ const main = async () => {
         }
     }
 
+    //libp2p	
     const libp2p = await createLibp2p(Libp2pOptions)
 
     libp2p.addEventListener('peer:discovery', (evt) => {
@@ -96,9 +111,13 @@ const main = async () => {
         //console.log('Connected to %s', evt.detail.toString()) // Log connected peer
     })
 
+    //helia
     const blockstore = new LevelBlockstore('./data/ipfs')
     const ipfs = await createHelia({ libp2p: libp2p, blockstore: blockstore, datastore: leveldatastore })   
-    //const fs = unixfs(ipfs)    
+    const fs = unixfs(ipfs)
+
+    
+    //orbit db    
     const orbitdb = await createOrbitDB({ ipfs, directory: './data/orbitdb' })
     let db
 
@@ -118,9 +137,22 @@ const main = async () => {
         console.log('db address:',  db.address)
     }
 
+    //Replication event
     db.events.on('update', async (entry) => {
         // what has been updated.
-        console.log('db updated:',  entry.payload.value)
+        let record = entry.payload.value
+        console.log('db updated:',  record)
+        let cid = record.cid
+        const decoder = new TextDecoder()
+	let text = ''
+        // use Helia node to fetch the file from the remote Helia node
+	for await (const chunk of fs.cat(cid)) {
+  			text += decoder.decode(chunk, {
+			stream: true
+		})
+	}
+
+	console.log('Fetched file contents:', text)
     })
 
     // Clean up when stopping this app using ctrl+c
@@ -141,17 +173,19 @@ const main = async () => {
     console.log('Peer Id:', libp2p.peerId.toString());
     console.log('Peer address:', libp2p.getMultiaddrs().toString());
     
-    
-    //Put some object to Orbit
-    await db.put(libp2p.peerId.toString(), { peer: libp2p.peerId.toString(), text: randomText})
-    
+       
     //Put some file to IPFS
-    //const encoder = new TextEncoder() //
-    //const bytes = encoder.encode(randomText)
-
+    const encoder = new TextEncoder() //
+    const bytes = encoder.encode(randomText)
     // add the bytes to your node and receive a unique content identifier
-    //const cid = await fs.addBytes(bytes) //bytes could be uploaded directly over REST API
-    //console.log('Added file to IPFS:', cid.toString())   
+    const cid = await fs.addBytes(bytes) //TODO: bytes will be uploaded directly over REST API
+    console.log('Added file to IPFS:', cid.toString())  
+    
+    //Put our object to Orbit to key/value database. For this demo we use peerId as key.
+    //Object will contain some random text property and cid of uploaded file.
+    //Remote peer will retreive the file from IPFS on db replication event
+    await db.put(libp2p.peerId.toString(), { peer: libp2p.peerId.toString(), text: randomText, cid: cid})
+ 
 }
 
 main()
